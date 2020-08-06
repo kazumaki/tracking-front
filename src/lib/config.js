2fe5e5368f97c6ca592e5315030e5ef73def4186
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-08-06 18:53:06 -0300

    Update api url

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: 'https://enigmatic-hamlet-36375.herokuapp.com/',
+  API_BASE_URL: 'https://vinic-tracking-api.herokuapp.com/',
   API_TEST_URL: 'http://192.168.100.53:3001',
 };
 
